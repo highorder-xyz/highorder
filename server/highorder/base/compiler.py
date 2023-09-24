24bf3c86167163c602a33f35251b0c2526f5c71a
Wei Zhuo <zeaphoo@qq.com>
2023-09-24 22:25:13 +0800

    WIP: compiler

--- server/highorder/base/compiler.py
+++ server/highorder/base/compiler.py
@@ -4,26 +4,33 @@ from dataclasses import dataclass, field
 from typing import Dict, List
 import string
 from copy import deepcopy
+import json
 
 class TokenKind(Enum):
-    Unknown = 1
+    Null = 1
     StringLiteral = 2
     NumberLiteral = 3
     ColorLiteral = 4
     Identifier = 5
+    PropertyName = 6
+    BoolLiteral = 7
     LineBreak = 8
     Comment = 9
     LBracket = 10
     RBracket = 11
     LBrace = 12
     RBrace = 13
-    Colon = 14
-    Mulitply = 15
-    Division = 16
-    Plus = 17
-    Minus = 18
-    Equal = 19
-    Semicolon = 20
+    LParen = 14
+    RParen = 15
+    Colon = 16
+    Comma = 17
+
+    Mulitply = 21
+    Division = 22
+    Plus = 23
+    Minus = 24
+    Equal = 25
+    Semicolon = 26
 
 
 @dataclass
@@ -63,25 +70,23 @@ class TokenizeContext:
     is_number: bool = field(default=False)
 
 class NodeKind(Enum):
-    Unknown = 1
-    Oject = 2
-    Property = 3
-    Dict = 4
-    List = 5
-    LineBreak = 10
+    Root = 1
+    Null = 2
+    Bool = 3
+    String = 4
+    Number = 5
+    List = 6
+    Object = 7
 
 
 @dataclass
-class NodePosition:
-    start: int
-    end: int
-
-@dataclass
-class HolaNode:
+class SyntaxNode:
     kind: NodeKind
-    position: NodePosition
-    properties: Dict[str, HolaNode] = field(default_factory=dict())
-    children: List[HolaNode] = field(default_factory=list())
+    start_pos: CharPosition
+    end_pos: CharPosition
+    value: str = field(default_factory=str)
+    properties: Dict[str, SyntaxNode] = field(default_factory=dict())
+    children: List[SyntaxNode] = field(default_factory=list())
 
 
 ESCAPE_CHAR_MAP = {
@@ -139,6 +144,14 @@ class Tokenizer:
                     end_pos = deepcopy(pos)
                 ))
                 pos.move(1)
+            elif ch == ',':
+                tokens.append(Token(
+                    kind = TokenKind.Comma,
+                    value = ',',
+                    start_pos = deepcopy(pos),
+                    end_pos = deepcopy(pos)
+                ))
+                pos.move(1)
             elif ch == '[':
                 tokens.append(Token(
                     kind = TokenKind.LBracket,
@@ -158,7 +171,7 @@ class Tokenizer:
             elif ch == ';':
                 tokens.append(Token(
                     kind = TokenKind.Semicolon,
-                    value = ']',
+                    value = ';',
                     start_pos = deepcopy(pos),
                     end_pos = deepcopy(pos)
                 ))
@@ -201,9 +214,18 @@ class Tokenizer:
         pos.move(count)
         end_pos = deepcopy(pos)
         pos.move(1)
+        kind = TokenKind.Identifier
+        value = ''.join(chars)
+        if chars[0] in string.ascii_lowercase:
+            if value == 'true' or value == 'false':
+                kind = TokenKind.BoolLiteral
+            elif value == 'null':
+                kind = TokenKind.Null
+            else:
+                kind = TokenKind.PropertyName
         return Token(
-            kind = TokenKind.Identifier,
-            value = ''.join(chars),
+            kind = kind,
+            value = value,
             start_pos = start_pos,
             end_pos = end_pos
         )
@@ -321,3 +343,333 @@ class Tokenizer:
             start_pos = start_pos,
             end_pos = end_pos
         )
+
+
+class TokenStream:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.idx = 0
+
+    def peek(self):
+        if self.idx >= len(self.tokens):
+            return None
+        return self.tokens[self.idx]
+
+    def peek_next(self, num = 0):
+        if num < 0:
+            raise Exception(f'TokenStream.peek_next num must >= 0, but {num} given.')
+        new_idx = self.idx + num
+        if new_idx >= len(self.tokens):
+            return None
+        return self.tokens[new_idx]
+
+    def consume(self, kind):
+        old_idx = self.idx
+        idx = self.idx
+        if isinstance(kind, (list, tuple)):
+            kinds = kind
+        else:
+            kinds = [kind]
+        while idx < len(self.tokens) and self.tokens[idx].kind in kinds:
+            idx += 1
+        self.idx = idx
+        return self.idx - old_idx
+
+    def expect(self, token_kind):
+        token = self.peek()
+        if isinstance(token_kind, (list, tuple)):
+            expected_kinds =  token_kind
+        else:
+            expected_kinds = [token_kind]
+        expected = token and token.kind in expected_kinds
+        if not expected:
+            raise HolaSyntaxError(message = f'Expect Token(token_kind), But {token.kind} Found.', pos=token.start_pos)
+        else:
+            self.next()
+
+    def next(self):
+        self.idx += 1
+        if self.idx > len(self.tokens):
+            self.idx = len(self.tokens)
+
+    def move(self, steps):
+        if steps < 0:
+            raise Exception(f'TokenStream.move steps must >= 0, but {steps} given.')
+        self.idx += steps
+        if self.idx > len(self.tokens):
+            self.idx = len(self.tokens)
+
+    def eof(self):
+        return self.idx >= len(self.tokens)
+
+class HolaSyntaxError(Exception):
+    def __init__(self, message, pos):
+        self.message = message
+        self.pos = pos
+
+    def __str__(self):
+        return f'Syntax Error (Line = {self.pos.line}, Column: {self.pos.column}): {self.message}'
+
+class HolaSyntaxExpectError(Exception):
+    def __init__(self, expect, found):
+        self.pos = found.start_pos
+        if not isinstance(expect, (list, tuple)):
+            self.expect = [expect]
+        else:
+            self.expect = expect
+        self.found = found
+
+    def __str__(self):
+        expect_str = ', '.join(list(map(lambda x: str(x), self.expect)))
+        return f'''Syntax Error (Line = {self.pos.line}, Column: {self.pos.column}):
+    Expect token:  {expect_str}
+    Found token: k={self.found.kind}, v="{self.fond.value}"
+    '''
+
+
+class Parser:
+    def __init__(self):
+        pass
+
+    def parse(self, code):
+        tokenizer = Tokenizer()
+        tokens = TokenStream(tokenizer.tokenize(code))
+        root = SyntaxNode(
+            kind = NodeKind.Root,
+            start_pos = CharPosition(index = 0, line = 0, column = 0),
+            end_pos = CharPosition(index = -1, line = -1, column = -1),
+            value = '',
+            properties = {},
+            children = []
+        )
+        while not tokens.eof():
+            node = self.parse_object(tokens)
+            root.children.append(node)
+
+        return root
+
+    def parse_object(self, tokens):
+        token = tokens.peek()
+        name = ''
+        if token.kind == TokenKind.Identifier:
+            name = token.value
+            tokens.next()
+        elif token.kind == TokenKind.LBrace:
+            pass
+        else:
+            raise HolaSyntaxExpectError([TokenKind.Identifier, TokenKind.LBrace], token)
+
+        node = SyntaxNode(
+            kind = NodeKind.Object,
+            start_pos = deepcopy(token.start_pos),
+            end_pos = CharPosition(index=-1, line=-1, column=-1),
+            value = name,
+            properties = {},
+            children = []
+        )
+        tokens.expect(TokenKind.LBrace)
+        tokens.consume(TokenKind.LineBreak)
+        while not tokens.eof():
+            token = tokens.peek()
+            if token.kind == TokenKind.RBrace:
+                break
+            if token.kind in [TokenKind.PropertyName, TokenKind.StringLiteral]:
+                name, value = self.parse_property(self, tokens)
+                node.properties[name] = value
+                tokens.consume(TokenKind.LineBreak)
+            elif token.kind in TokenKind.Identifier:
+                sub_node = self.parse_object(tokens)
+                node.children.append(sub_node)
+            else:
+                raise HolaSyntaxExpectError(
+                    [TokenKind.StringLiteral, TokenKind.PropertyName, TokenKind.Identifier],
+                    token
+                )
+
+        print(tokens.peek())
+        token = tokens.peek()
+        node.end_pos = deepcopy(token.end_pos)
+        tokens.expect(TokenKind.RBrace)
+        return node
+
+    def parse_property(self, tokens):
+        token = tokens.peek()
+        name = token.value
+        tokens.next()
+        tokens.expect(TokenKind.Colon)
+        tokens.consume(TokenKind.LineBreak)
+        value = self.parse_value(self, tokens)
+        return (name, value)
+
+    def parse_value(self, tokens):
+        token = tokens.peek()
+        if token.kind == TokenKind.Null:
+            tokens.next()
+            return SyntaxNode(
+                kind = NodeKind.Null,
+                value = None,
+                start_pos = deepcopy(token.start_pos),
+                end_pos = deepcopy(token.end_pos)
+            )
+        elif token.kind == TokenKind.BoolLiteral:
+            tokens.next()
+            return SyntaxNode(
+                kind = NodeKind.Bool,
+                value = True if token.value == 'true' else False,
+                start_pos = deepcopy(token.start_pos),
+                end_pos = deepcopy(token.end_pos)
+            )
+
+        elif token.kind == TokenKind.StringLiteral:
+            tokens.next()
+            return SyntaxNode(
+                kind = NodeKind.String,
+                value = token.value,
+                start_pos = deepcopy(token.start_pos),
+                end_pos = deepcopy(token.end_pos)
+            )
+
+        elif token.kind == TokenKind.NumberLiteral:
+            tokens.next()
+            return SyntaxNode(
+                kind = NodeKind.Number,
+                value = token.value,
+                start_pos = deepcopy(token.start_pos),
+                end_pos = deepcopy(token.end_pos)
+            )
+
+        elif token.kind == TokenKind.LBracket:
+            return self.parse_list(tokens)
+        elif token.kind == TokenKind.Identifier or token.kind == TokenKind.LBrace:
+            return self.parse_object(tokens)
+        else:
+            raise HolaSyntaxError(message=f'Unknown To Handle Token Kind {token.kind} and {token.value}')
+
+    def parse_list(self, tokens):
+        token = tokens.peek()
+        node = SyntaxNode(
+            kind = NodeKind.List,
+            start_pos = deepcopy(token.start_pos),
+            end_pos = CharPosition(index=-1, line=-1, column=-1),
+            value = None,
+            properties = {},
+            children = []
+        )
+        tokens.expect(TokenKind.LBracket)
+        tokens.consume(TokenKind.LineBreak)
+
+        while not tokens.eof():
+            token = tokens.peek()
+            if token.kind == TokenKind.RBracket:
+                break
+            if token.kind == TokenKind.Comma:
+                node.children.append(node = SyntaxNode(
+                    kind = NodeKind.Null,
+                    start_pos = deepcopy(token.start_pos),
+                    end_pos = deepcopy(token.start_pos),
+                    value = None,
+                    properties = {},
+                    children = []
+                ))
+                continue
+            else:
+                sub_node = self.parse_value(tokens)
+                tokens.expect([TokenKind.Comma, TokenKind.LineBreak])
+                tokens.consume(TokenKind.LineBreak)
+                node.children.append(sub_node)
+        token = tokens.peek()
+        node.end_pos = deepcopy(token.end_pos)
+        tokens.expect(TokenKind.RBracket)
+        return node
+
+CHILD_PROPERTY_NAMES = {
+    "data-object": "properties"
+}
+
+class JsonCodeGenerator:
+    def __init__(self):
+        pass
+
+    def gen(self, node):
+        raw_obj_list = []
+        for sub_node in node.children:
+            assert sub_node.kind == NodeKind.Object
+            obj = self.gen_object(sub_node)
+            raw_obj_list.append(obj)
+        json_obj_root = {
+            "attributes": [],
+        }
+
+        for obj in raw_obj_list:
+            obj_type = obj["type"]
+            if obj_type in [""]:
+                pass
+            else:
+                raise Exception()
+        return json_obj_root
+
+    def transform_obj_name(self, name):
+        if not name:
+            return name
+        chars = []
+        chars.append(name[0].lower())
+        for char in name[1:]:
+            if char in string.ascii_uppercase:
+                chars.append('_')
+                chars.append(char.low())
+            else:
+                chars.append(char)
+        return ''.join(name)
+
+    def gen_object(self, node):
+        obj = {}
+        if node.value.lower() != "object":
+            obj["type"] = self.transform_obj_name(node.value)
+        for k, v in node.properties.items():
+            if k.lower() == "type":
+                continue
+            obj[k] = self.gen_value(v)
+
+        if obj.children:
+            child_key_name = CHILD_PROPERTY_NAMES.get(obj["type"], "elements")
+            obj[child_key_name] = []
+            for child in obj.children:
+                obj[child_key_name].append(self.gen_value(child))
+
+        return obj
+
+    def gen_list(self, node):
+        _list = []
+        for sub_node in node.children:
+            _list.append(self.gen_value(sub_node))
+        return _list
+
+    def gen_value(self, node):
+        if node.kind == NodeKind.Null:
+            return None
+        elif node.kind == NodeKind.Bool:
+            return node.value
+        elif node.kind == NodeKind.Number:
+            return node.value
+        elif node.kind == NodeKind.String:
+            return node.value
+        elif node.kind == NodeKind.List:
+            return self.gen_list(node)
+        elif node.kind == NodeKind.Object:
+            return self.gen_object(node)
+        else:
+            raise Exception('no generator function for Node(kind={node.kind})')
+
+
+class Compiler:
+    def __init__(self):
+        pass
+
+    def compile(self, code, target="json"):
+        parser = Parser()
+        node = parser.parse(code)
+        if target == 'json':
+            generator = JsonCodeGenerator()
+            return generator.gen(node)
+        else:
+            raise Exception(f'Only target with json supported.')
--- server/tests/test_compiler.py
+++ server/tests/test_compiler.py
@@ -1,10 +1,15 @@
 
-from highorder.base.compiler import Tokenizer, TokenKind
+from highorder.base.compiler import Tokenizer, TokenKind, NodeKind, Parser
 
 def compare_tokens(code, desire_tokens):
     raw_tokens = Tokenizer().tokenize(code)
     tokens = list(map(lambda x: (x.kind, x.value), raw_tokens))
     assert tokens == desire_tokens
+
+def compare_nodes(code, desire_nodes):
+    root_node = Parser().parse(code)
+    nodes = list(map(lambda x: {'kind': x.kind, 'value': x.value, 'properties': x.properties, 'children':x.children}, root_node.children))
+    assert desire_nodes == desire_nodes
 
 def test_tokenizer_simple_1():
     compare_tokens(
@@ -28,7 +33,7 @@ def test_tokenizer_simple_3():
         [
             (TokenKind.Identifier, 'Page'),
             (TokenKind.LBrace, '{'),
-            (TokenKind.Identifier, 'route'),
+            (TokenKind.PropertyName, 'route'),
             (TokenKind.Colon, ':'),
             (TokenKind.StringLiteral, '/'),
             (TokenKind.RBrace, '}')
@@ -42,7 +47,7 @@ bb" }''',
         [
             (TokenKind.Identifier, 'Page'),
             (TokenKind.LBrace, '{'),
-            (TokenKind.Identifier, 'route'),
+            (TokenKind.PropertyName, 'route'),
             (TokenKind.Colon, ':'),
             (TokenKind.StringLiteral, '/a\nbb'),
             (TokenKind.RBrace, '}')
@@ -57,13 +62,27 @@ def test_tokenizer_simple_4():
         [
             (TokenKind.Identifier, 'Page'),
             (TokenKind.LBrace, '{'),
-            (TokenKind.Identifier, 'route'),
+            (TokenKind.PropertyName, 'route'),
             (TokenKind.Colon, ':'),
             (TokenKind.StringLiteral, '/'),
             (TokenKind.LineBreak, '\n'),
-            (TokenKind.Identifier, 'size'),
+            (TokenKind.PropertyName, 'size'),
             (TokenKind.Colon, ':'),
             (TokenKind.NumberLiteral, 5),
             (TokenKind.RBrace, '}')
+        ]
+    )
+
+
+def test_parser_simple_1():
+    compare_nodes(
+        '''Page {}''',
+        [
+            {
+                "kind": NodeKind.Object,
+                "value": "Page",
+                "properties": {},
+                "children":[]
+            }
         ]
     )
