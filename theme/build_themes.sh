7a8aa9d8aadaeab6881c3cea3a9f3496fbfb36aa
Wei Zhuo <zeaphoo@qq.com>
2023-10-25 23:34:37 +0800

    add editor and theme

---
+++ theme/build_themes.sh
@@ -0,0 +1,19 @@
+sass --update themes/retro/retro-light/theme.scss:dist/themes/retro-light/theme.css
+sass --update themes/fluent/fluent-light/theme.scss:dist/themes/fluent-light/theme.css
+sass --update themes/tailwind/tailwind-light/theme.scss:dist/themes/tailwind-light/theme.css
+
+sass --update themes/material/material-dark/standard/deeppurple/theme.scss:dist/themes/md-dark-deeppurple/theme.css
+sass --update themes/material/material-dark/standard/indigo/theme.scss:dist/themes/md-dark-indigo/theme.css
+
+sass --update themes/material/material-light/standard/deeppurple/theme.scss:dist/themes/md-light-deeppurple/theme.css
+sass --update themes/material/material-light/standard/indigo/theme.scss:dist/themes/md-light-indigo/theme.css
+
+sass --update themes/lara/lara-dark/blue/theme.scss:dist/themes/lara-dark-blue/theme.css
+sass --update themes/lara/lara-dark/indigo/theme.scss:dist/themes/lara-dark-indigo/theme.css
+sass --update themes/lara/lara-dark/purple/theme.scss:dist/themes/lara-dark-purple/theme.css
+sass --update themes/lara/lara-dark/teal/theme.scss:dist/themes/lara-dark-teal/theme.css
+
+sass --update themes/lara/lara-light/blue/theme.scss:dist/themes/lara-light-blue/theme.css
+sass --update themes/lara/lara-light/indigo/theme.scss:dist/themes/lara-light-indigo/theme.css
+sass --update themes/lara/lara-light/purple/theme.scss:dist/themes/lara-light-purple/theme.css
+sass --update themes/lara/lara-light/teal/theme.scss:dist/themes/lara-light-teal/theme.css
